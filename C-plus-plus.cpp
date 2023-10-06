02a9e66c5247376e825975e7290cb8e88bd10393
Dylan <dmedjnou@student.ubc.ca>
2023-10-06 21:42:55 +0000

    added a random cpp file
    C++ :)

---
+++ C-plus-plus.cpp
@@ -0,0 +1,20 @@
+// ---
+// jupyter:
+//   jupytext:
+//     text_representation:
+//       extension: .cpp
+//       format_name: light
+//       format_version: '1.5'
+//       jupytext_version: 1.14.5
+//   kernelspec:
+//     display_name: C++17
+//     language: C++17
+//     name: xcpp17
+// ---
+
+// +
+#include <iostream>
+
+using namespace std;
+
+cout << "Hello World";
